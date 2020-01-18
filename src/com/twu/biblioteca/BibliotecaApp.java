f9defb03c6052818148c55673e2f446e264aad1b
Victoria Alves <victoria.alves@thoughtworks.com>
2020-01-17 22:14:34 -0300

    1.1 View a Welcome Message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,15 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, world!");
+    }
+
+    public String getWelcomeMessage(String[] args) {
+        String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+
+        System.out.println(welcomeMessage);
+
+        return welcomeMessage;
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaAppTest {
+
+    @Test
+    public void displaysWelcomeMessageWhenStartingApplication() {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+
+        assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!",
+                bibliotecaApp.getWelcomeMessage(new String[100]));
+    }
+}
