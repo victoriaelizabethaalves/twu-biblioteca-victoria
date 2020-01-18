fb2c3d168cc4c3e4acea1062bbe829273dce2ae8
Victoria Alves <victoria.alves@thoughtworks.com>
2020-01-17 22:30:07 -0300

    1.1 Refactor method name

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,7 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
-    public String main(String[] args) {
+    public String mainService(String[] args) {
         String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
 
         System.out.println(welcomeMessage);
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -11,6 +11,6 @@ public class BibliotecaAppTest {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
 
         assertEquals("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!",
-                bibliotecaApp.main(new String[100]));
+                bibliotecaApp.mainService(new String[100]));
     }
 }
